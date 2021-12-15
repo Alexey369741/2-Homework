88fb727cbd590adc3c6ffe73307863c00f11d0db
Alexey Maistrenko <85947242+Alexey369741@users.noreply.github.com>
2021-12-15 22:15:22 +0200

    changes

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const mathAction = prompt ('What operation do you want to do?', '+ - / *');
+const mathAction = prompt ('What operation do you want to do?', '+ - * /');
 const firstNum = +prompt("Enter the first number", "");
 const secondNum = +prompt("Enter the second number", "");
+
 function add(mathAction) {
     if (mathAction === "+") {
         alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum + secondNum}`);
-        return;
+        return firstNum + secondNum;
     }
 }
+
 function sub(mathAction) {
-    if (mathAction === "*") {
-        alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum * secondNum}`);
-        return;
-    }
-}
-function mult(mathAction) {
     if (mathAction === "-") {
-        alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum - secondNum}`);
-        return;
+        alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum * secondNum}`);
+        return firstNum - secondNum;
     }
 }
+
+function mult(mathAction) {
+    if (mathAction === "*") {
+        alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum - secondNum}`);
+        return firstNum * secondNum;
+    }
+}
+
 function div(mathAction) {
     if (mathAction === "/") {
         alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum / secondNum}`);
-        return;
+        return firstNum / secondNum;
     }
 }
-const resultAdd = add(mathAction, firstNum, secondNum);
-const resultSub = sub(mathAction, firstNum, secondNum);
-const resultMult = mult(mathAction, firstNum, secondNum);
-const resultDiv = div(mathAction, firstNum, secondNum);
