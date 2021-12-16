0b1d2eda1effbbab2232c0254726e750c044d3a8
Alexey Maistrenko <85947242+Alexey369741@users.noreply.github.com>
2021-12-16 15:05:56 +0200

    changes

--- script.js
+++ script.js
@@ -2,30 +2,27 @@ const mathAction = prompt ('What operation do you want to do?', '+ - * /');
 const firstNum = +prompt("Enter the first number", "");
 const secondNum = +prompt("Enter the second number", "");
 
-function add(mathAction) {
-    if (mathAction === "+") {
-        alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum + secondNum}`);
-        return firstNum + secondNum;
-    }
+function add(firstNum,secondNum) {
+    return firstNum + secondNum;
 }
-
-function sub(mathAction) {
-    if (mathAction === "-") {
-        alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum * secondNum}`);
+    if (mathAction === "+") {
+        alert(`${firstNum} ${mathAction} ${secondNum} = ${add(firstNum,secondNum)}`);
+    }
+function sub(firstNum,secondNum) {
         return firstNum - secondNum;
     }
-}
-
+    if (mathAction === "-") {
+        alert(`${firstNum} ${mathAction} ${secondNum} = ${sub(firstNum,secondNum)}`);
+    }
 function mult(mathAction) {
-    if (mathAction === "*") {
-        alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum - secondNum}`);
         return firstNum * secondNum;
     }
-}
-
-function div(mathAction) {
-    if (mathAction === "/") {
-        alert(`${firstNum} ${mathAction} ${secondNum} = ${firstNum / secondNum}`);
-        return firstNum / secondNum;
+    if (mathAction === "*") {
+        alert(`${firstNum} ${mathAction} ${secondNum} = ${mult(firstNum,secondNum)}`);
     }
-}
+function div(mathAction) {
+        return firstNum / secondNum
+    }
+    if (mathAction === "/") {
+        alert(`${firstNum} ${mathAction} ${secondNum} = ${div(firstNum,secondNum)}`);
+    }
